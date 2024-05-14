4e5bdcd653160a2ff2093791be36e4ceafa70928
Beltz <humberto.c.lima18@gmail.com>
2024-05-14 20:01:20 -0300

    adiciona scripts `lint:check` e `lint:fix`

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
-function Home(){
-    return <h1>Se vc tá sem calcinha, dê uma risadinha. </h1>
+function Home() {
+  return <h1>Se vc tá sem calcinha, dê uma risadinha. </h1>;
 }
 
 export default Home;
